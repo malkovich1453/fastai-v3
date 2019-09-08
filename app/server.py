1b31c4114264ebc74be6b5829cd721f62bff52be
malkovich1453 <48166572+malkovich1453@users.noreply.github.com>
2019-09-08 13:56:47 +0900

    Update server.py

--- app/server.py
+++ app/server.py
@@ -1,4 +1,3 @@
-import os
 import aiohttp
 import asyncio
 import uvicorn
@@ -10,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'os.getenv(pickle_from_ggl)'
+export_file_url = 'pickle_from_ggl'
 export_file_name = 'exportv2.pkl'
 
 classes = ['miso', 'shio', 'shoyu', 'tonkotsu']
